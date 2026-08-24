e6c3a25b7c704077f22bd35753ee7503b0d1ca99
modengann <99417989+modengann@users.noreply.github.com>
2026-08-24 17:54:01 -0400

    Initial commit

---
+++ src/mystery_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+def mystery_data():
+    pass
+
+def main():
+    coefficients = mystery_data()
+    # print the coefficients here
+    print(f"Coefficient of X1 is {coefficients[0]}")
+    print(f"Coefficient of X2 is {coefficients[1]}")
+    print(f"Coefficient of X3 is {coefficients[2]}")
+    print(f"Coefficient of X4 is {coefficients[3]}")
+    print(f"Coefficient of X5 is {coefficients[4]}")
+    
+if __name__ == "__main__":
+    main()
